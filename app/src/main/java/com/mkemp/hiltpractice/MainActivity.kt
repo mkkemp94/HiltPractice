2d700935cc07bbea905a63468423b6d657ca6664
MKemp <mkemp@accu-time.com>
2020-09-15 13:09:45 -0400

    Provide interfaces and external dependencies

--- app/src/main/java/com/mkemp/hiltpractice/MainActivity.kt
+++ app/src/main/java/com/mkemp/hiltpractice/MainActivity.kt
@@ -2,10 +2,19 @@ package com.mkemp.hiltpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.AppComponentFactory
 import androidx.fragment.app.Fragment
 import com.google.gson.Gson
+import dagger.Binds
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
 import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.components.ActivityComponent
+import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.android.scopes.ActivityScoped
 import javax.inject.Inject
+import javax.inject.Singleton
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity()
@@ -48,4 +57,23 @@ constructor() : SomeInterface
 interface SomeInterface
 {
     fun getAThing(): String
+}
+
+@InstallIn(ApplicationComponent::class)
+@Module
+class MyModule
+{
+    @Singleton
+    @Provides
+    fun provideSomeInterfaceImpl(): SomeInterface
+    {
+        return SomeInterfaceImpl()
+    }
+    
+    @Singleton
+    @Provides
+    fun provideGson(): Gson
+    {
+        return Gson()
+    }
 }
