0fcd43edc3f3e6251521c120290636b050bace52
MKemp <mkemp@accu-time.com>
2020-09-15 11:27:14 -0400

    Show that we can't inject interfaces or third party classes without inject

--- app/src/main/java/com/mkemp/hiltpractice/MainActivity.kt
+++ app/src/main/java/com/mkemp/hiltpractice/MainActivity.kt
@@ -1,17 +1,16 @@
 package com.mkemp.hiltpractice
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 import androidx.fragment.app.Fragment
+import com.google.gson.Gson
 import dagger.hilt.android.AndroidEntryPoint
-import dagger.hilt.android.scopes.ActivityScoped
 import javax.inject.Inject
-import javax.inject.Singleton
 
-@AndroidEntryPoint // No need to declare main activity in the component. Just use this annotation.
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity()
 {
-    @Inject // Field injection
+    @Inject
     lateinit var someClass: SomeClass
     
     override fun onCreate(savedInstanceState: Bundle?)
@@ -23,21 +22,30 @@ class MainActivity : AppCompatActivity()
     }
 }
 
-@AndroidEntryPoint
-class MyFragment: Fragment()
-{
-    @Inject
-    lateinit var someClass: SomeClass
-}
-
-// Created at compile time
-@ActivityScoped // @Singleton will also work but @FragmentScope will NOT
 class SomeClass
 @Inject
-constructor()
+constructor(
+        private val someInterfaceImpl: SomeInterface, // Cannot do constructor injection when injecting an interface...
+        private val gson: Gson // Can't inject this because I don't own it!!
+)
 {
     fun doAThing(): String
     {
-        return "Look, I did a thing!"
+        return "Look I got: ${someInterfaceImpl.getAThing()}"
     }
+}
+
+class SomeInterfaceImpl
+@Inject
+constructor() : SomeInterface
+{
+    override fun getAThing(): String
+    {
+        return "A Thing"
+    }
+}
+
+interface SomeInterface
+{
+    fun getAThing(): String
 }
